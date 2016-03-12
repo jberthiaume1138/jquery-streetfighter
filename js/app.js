9790ecab6661f95f50e446040c147978ba060435
jberthiaume1138 <joenyc23@gmail.com>
2016-03-12 03:27:09 -0500

    keypress and animation complete

--- js/app.js
+++ js/app.js
@@ -3,22 +3,61 @@ $(document).ready(function() {
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
 	})
-	$('.ryu').mouseleave(function() {
+	.mouseleave(function() {
 		$('.ryu-still').show();
 		$('.ryu-ready').hide();	
 	})
-	$('.ryu').mousedown(function() {
+	.mousedown(function() {
+		playHadouken();
 		$('.ryu-throwing').show();
 		$('.ryu-still').hide();
 		$('.ryu-ready').hide();	
-		$('.hadouken').show();
-		// play sound
-		// animate Hadouken
+		$('.hadouken').finish().show().animate(
+			{'left':'1020px'},
+			500,
+			function() {
+				$(this).hide();
+				$(this).css('left','515px');
+			}
+		)
 	})
-	$('.ryu').mouseup(function() {
+	.mouseup(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();	
-		// $('.hadouken').hide();
-	});
+	})
+
+ 	$(document).keydown(function(e) {
+ 		if (e.which == 88 ) {
+			console.log("key=" + event.keyCode);
+			coolRyu();
+    	}
+    })
+
+ 	$(document).keyup(function(e) {
+ 		if (e.which ==88 ) {
+ 			console.log("key=" + event.keyCode + "up");
+ 			stillRyu();
+ 		}
+ 	});
 });
+
+function coolRyu () {
+	$('.ryu-cool').show();
+	$('.ryu-throwing').hide();
+	$('.ryu-still').hide();
+	$('.ryu-ready').hide();
+}
+
+function stillRyu () {
+	$('.ryu-cool').hide();
+	$('.ryu-throwing').hide();
+	$('.ryu-still').show();
+	$('.ryu-ready').hide();
+}
+
+function playHadouken () {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
